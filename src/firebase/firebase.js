12e738405845ed7269e981cd7a6500208487dc45
elam126 <elamparithi@venzotechnologies.com>
2024-07-25 12:01:03 +0530

    font chaange, firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import {getDatabase} from 'firebase/database';
 import {getStorage} from 'firebase/storage';
-import { 
-  REACT_APP_FIREBASE_API_KEY, 
-  REACT_APP_FIREBASE_AUTH_DOMAIN, 
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
   REACT_APP_FIREBASE_PROJECT_ID,
   REACT_APP_FIREBASE_STORAGE_BUCKET,
   REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID
+  REACT_APP_FIREBASE_APP_ID,
 } from '@env';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,12 +18,15 @@ import {
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCbAHuyLzXWGP0YpGdyKAUMhDxNuMNQmM8',
+  authDomain: 'venzoturfbooking.firebaseapp.com',
+  databaseURL:
+    'https://venzoturfbooking-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'venzoturfbooking',
+  storageBucket: 'venzoturfbooking.appspot.com',
+  messagingSenderId: '744304593354',
+  appId: '1:744304593354:web:83703623a3b45435296733',
+  measurementId: 'G-2K9NZS11N9',
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
