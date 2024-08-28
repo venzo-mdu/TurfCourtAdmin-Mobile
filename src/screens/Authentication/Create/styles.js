d132c11281acc7575f76d1b0a67e2bab068cd4dd
santhiya107 <santhiyak@venzotechnologies.com>
2024-08-28 15:59:57 +0530

    signup fix

--- src/screens/Authentication/Create/styles.js
+++ src/screens/Authentication/Create/styles.js
@@ -157,23 +157,6 @@ export const UserLoginScreenStyles = StyleSheet.create({
     marginBottom: hS(10),
     paddingBottom: 100,
   },
-  //   loginBox: {
-  //     backgroundColor: COLORS.PRIMARY,
-  //     width: mS(150),
-  //     height: mS(50),
-  //     alignSelf: "center",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //     marginBottom: hS(20),
-  //     borderRadius: mS(8),
-  //   },
-  //   loginTxt: {
-  //     fontSize: mS(18),
-  //     color: COLORS.WHITE,
-  //     fontFamily: PoppinsMedium,
-  //   },
-
-  /*New */
   loginBox: {
     backgroundColor: COLORS.buttonColor,
     width: '100%',
